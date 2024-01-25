4562f3eb2560dbb0f627f0527a6b883c72f1ec9c
Aasim Ajaz <95701475+aasimajaz@users.noreply.github.com>
2024-01-25 09:41:40 +0300

    Update vm_external_ip.tf

--- vm_external_ip.tf
+++ vm_external_ip.tf
@@ -37,7 +37,7 @@ resource "google_compute_instance" "default" {
   network_interface {
     network = "default"
     access_config {
-      nat_ip = google_compute_address.default.address
+      ...
     }
   }
 }
