dfc6398f00b58a2d04957b945b8696f144e07204
mashael1998 <68636673+mashael1998@users.noreply.github.com>
2024-02-11 13:33:12 +0300

    Update misconfig-secrets.tf

--- misconfig-secrets.tf
+++ misconfig-secrets.tf
@@ -1,6 +1,20 @@
-resource "google_compute_address" "default" {
-  name   = "my-test-static-ip-address"
-  region = "us-central1"
+provider "google" {
+  credentials = file("service-account.json")
+  project     = "your-project-id"
+  region      = "us-central1"
 }
 
-variable "database_password" { type = string description = "Password for the database" } resource "google_sql_database_instance" "example_instance" { name = "example-db-instance" database_version = "MYSQL_5_7" settings { tier = "db-n1-standard-1" } deletion_protection = false disk_type = "PD_SSD" } resource "google_sql_user" "example_user" { name = "example-user" instance = google_sql_database_instance.example_instance.name password = var.database_password # Not handling secrets properly } output "example_output" { value = google_sql_database_instance.example_instance.connection_name }
+resource "google_compute_instance" "example" {
+  name         = "example-instance"
+  machine_type = "n1-standard-1"
+  zone         = "us-central1-a"
+  
+  metadata_startup_script = <<-EOF
+    #!/bin/bash
+    echo "Password123" > /etc/my_app_password
+  EOF
+}
+
+output "instance_password" {
+  value = "Password123"
+}
